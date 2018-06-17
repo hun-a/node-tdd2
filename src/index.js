cdf4c49519c305d2ec592350c8823d3e5ed0d410
seunghun <seunghun.chan@gmail.com>
2018-06-17 14:23:26 +0900

    [refactoring] added router to the index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import users from './api/users';
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', users);
 
 export default app;
